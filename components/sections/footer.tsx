77391b4372f0b5c95376a2cb7ee97e02f6631a60
Bryan Graham <iampapagray@gmail.com>
2023-09-06 20:13:27 +0000

    Minor changes

--- components/sections/footer.tsx
+++ components/sections/footer.tsx
@@ -8,23 +8,23 @@ import { Icons } from "../icons"
 
 export function Footer() {
   return (
-    <div className="flex flex-col w-full relative border-b">
+    <div className="relative flex w-full flex-col border-b">
       <div
         className={cn(
-          "flex justify-between items-center border-b border-gray-400 py-4",
+          "flex items-center justify-between border-b border-gray-400 py-4",
           styles.paddingX
         )}
       >
         <p className="flex items-center text-sm lg:text-base ">
-          made with <Icons.love className="h-4 w-4 mx-2 text-primary" /> by
-          <Link href="https://iampapagray.com" className="text-primary pl-2">
+          made with <Icons.love className="mx-2 h-4 w-4 text-primary" /> by
+          <Link href="https://iampapagray.com" className="pl-2 text-primary">
             iampapagray
           </Link>
         </p>
 
         <Icons.logo className="h-6 w-6" />
       </div>
-      <div className="flex items-center text-center text-sm lg:text-base justify-center py-4">
+      <div className="flex items-center justify-center py-4 text-center text-sm lg:text-base">
         Copyright &copy; {new Date().getFullYear()} The She Guards. All rights
         reserved.
       </div>
